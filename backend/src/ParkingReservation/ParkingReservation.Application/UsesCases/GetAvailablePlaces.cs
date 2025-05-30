79b6d7c08e0a9f686c88c8d84b59c713d09f13bf
xororist <hcachon@myges.fr>
2025-05-30 14:46:56 +0200

    refactoring GET /avaible-places to expose dto instead of domain model & unit test

--- backend/src/ParkingReservation/ParkingReservation.Application/UsesCases/GetAvailablePlaces.cs
+++ backend/src/ParkingReservation/ParkingReservation.Application/UsesCases/GetAvailablePlaces.cs
@@ -5,13 +5,19 @@ namespace ParkingReservation.Application.UsesCases;
 
 public interface IGetAvailablePlaces
 {
-    IEnumerable<ParkingLot> Handle();
+    IEnumerable<AvailableParkingLotsInfo> Handle();
 }
+
+public class AvailableParkingLotsInfo(char row, int column, bool isAvailable);
 
 public class GetAvailablePlaces(IQueryAvailablePlaces query) : IGetAvailablePlaces
 {
-    public IEnumerable<ParkingLot> Handle()
+    public IEnumerable<AvailableParkingLotsInfo> Handle()
     {
-        return query.GetAvailablePlaces();
+        return query.GetAvailablePlaces().Select(x => 
+            new AvailableParkingLotsInfo(
+                row:x.Row, 
+                column:x.Column, 
+                isAvailable:x.IsAvailable));
     }
 }
